09af5c83737dfc1c54d6ba9771f78cc0a88a6c34
Ivan <morgantdarck@gmail.com>
2019-10-12 12:59:08 +0300

    - работа над индикатором (новые элементы)

--- src/ru/phoenix/game/hud/assembled/SelfIndicators.java
+++ src/ru/phoenix/game/hud/assembled/SelfIndicators.java
@@ -9,20 +9,32 @@ public class SelfIndicators {
     private Board mainBoard;
     private Board firstPointIndicator;
     private Board secondPointIndicator;
+    private Board healthIndicator;
+    private Board mannaIndicator;
+    private Board staminaIndicator;
 
     public SelfIndicators(float width,Vector3f offsetPosition){
         mainBoard = new Indicator("./data/content/texture/boards/personal_indicator.png",width,offsetPosition);
         firstPointIndicator = new Indicator("./data/content/texture/boards/1_st_point.png",width,offsetPosition);
         secondPointIndicator = new Indicator("./data/content/texture/boards/2_st_point.png",width,offsetPosition);
+        healthIndicator = new Indicator("./data/content/texture/boards/health.png",width * 58.064f / 100.0f,offsetPosition.add(new Vector3f(0.061f,0.0f,0.0f)));
+        mannaIndicator = new Indicator("./data/content/texture/boards/manna.png",width* 58.064f / 100.0f,offsetPosition.add(new Vector3f(0.061f,0.0f,0.0f)));
+        staminaIndicator = new Indicator("./data/content/texture/boards/stamina.png",width* 58.064f / 100.0f,offsetPosition.add(new Vector3f(0.061f,0.0f,0.0f)));
     }
 
     public void update(Vector3f currentPosition){
         mainBoard.update(currentPosition);
         firstPointIndicator.update(currentPosition);
         secondPointIndicator.update(currentPosition);
+        healthIndicator.update(currentPosition);
+        mannaIndicator.update(currentPosition);
+        staminaIndicator.update(currentPosition);
     }
 
     public void draw(Shader shader){
+        healthIndicator.draw(shader);
+        mannaIndicator.draw(shader);
+        staminaIndicator.draw(shader);
         firstPointIndicator.draw(shader);
         secondPointIndicator.draw(shader);
         mainBoard.draw(shader);
